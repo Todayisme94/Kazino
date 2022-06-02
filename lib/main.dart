faa9e2ef3377cbd345bf6269fc8231a3d05c900a
Fauzan312 <fauzanadrivano312@users.noreply.github.com>
2022-06-02 14:52:54 +0700

    master

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'LoginPage.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -11,8 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
-      debugShowCheckedModeBanner: false,
-      home: const LoginPage());
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        debugShowCheckedModeBanner: false,
+        home: const LoginPage());
   }
 }
